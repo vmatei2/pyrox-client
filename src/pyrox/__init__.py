8008c5f9a4a2aa9f6715316ccfc4aa8aac47d793
Vlad Matei <vlad.mateei@gmail.com>
2025-08-24 17:54:34 +0100

    bump version

--- src/pyrox/__init__.py
+++ src/pyrox/__init__.py
@@ -2,4 +2,4 @@ from .core import get_race, list_races
 from .config import set_bucket, set_manifest_path, get_config
 
 __all__ = ["get_race", "list_races", "set_bucket", "set_manifest_path", "get_config"]
-__version__ = "0.0.1"
+__version__ = "0.0.2"
