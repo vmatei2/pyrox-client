ef10a9f1e8ffa3851f2fee8cbdc072aef351054f
Vlad Matei <vlad.mateei@gmail.com>
2025-11-30 12:39:31 +0000

    add e2e test for retrieving athlete

--- tests/test_client_e2e.py
+++ tests/test_client_e2e.py
@@ -4,11 +4,12 @@ import pandas as pd
 import pytest
 
 from src.pyrox.core import PyroxClient
+from src.pyrox.errors import AthleteNotFound
 
 @pytest.mark.integration
 def test_get_season_somke():
     """
-    Very simple end-to-end smoke test against the live CDN.
+    Very simple end-to-end get_season smoke test against the live CDN.
     """
 
     client = PyroxClient()
@@ -21,3 +22,18 @@ def test_get_season_somke():
     assert (set(df['gender'])) == set(['male', 'mixed', 'female'])
     assert len(df) > 1000
 
+@pytest.mark.integration
+def test_get_athlete_smoke():
+    """
+    Very simple get athlete end-to-end smoke test against the live cdn"""
+
+    client = PyroxClient()
+    df = client.get_athlete_in_race(season=7, location="Barcelona", athlete_name="Matei")
+
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) == 1
+    assert df['age_group'][0] == '25-29'
+    assert df['wallBalls_time'][0] > 6.0 # :( 
+
+    with pytest.raises(AthleteNotFound):
+        df = client.get_athlete_in_race(season=7, location="Barcelona", athlete_name="NonExistentAthlete123")
