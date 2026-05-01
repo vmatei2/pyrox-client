6c62b1e45707f523b21cf08627df7125df2fcf67
Vlad Matei <vlad.mateei@gmail.com>
2026-05-01 10:41:11 +0100

    Update tp streamline client release

--- src/pyrox/__init__.py
+++ src/pyrox/__init__.py
@@ -2,14 +2,12 @@
 
 from .core import PyroxClient
 from .errors import AthleteNotFound, PyroxError, RaceNotFound
-from .reporting import ReportingClient
 
 __version__ = "0.2.3"
 
 
 __all__ = [
     "PyroxClient",
-    "ReportingClient",
     "PyroxError",
     "RaceNotFound",
     "AthleteNotFound",
